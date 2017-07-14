14510492fc391ee3ccc88503e0559f5918227241
Givan33 <igaspar33@gmail.com>
2017-07-14 11:46:29 -0500

    Se agrego la clase fruta y se modifico la logica del juego

--- GameTools.py
+++ GameTools.py
@@ -16,11 +16,14 @@ WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
-
+SPEED = 1
 PLAYER_SIZE = 15
-BANANA_SIZE = 10
 PLAYER_SPEED = 1
+PLAYER_JUMP_SIZE = 50
+BANANA_SIZE = 10
 BANANA_SPEED = 2
+COCO_SIZE = 10
+COCO_SPEED = 4
 SCREEN_WIDTH = 640
 SCREEN_HEIGHT = 480 
 SCREEN_SIZE = (SCREEN_WIDTH, SCREEN_HEIGHT)
@@ -32,67 +35,115 @@ class Player:
         self.screen = screen
         self.x = x
         self.y = y
-        self.speed = 1
+        self.isJump = False
         
     def dibuja(self):
         pygame.draw.circle(self.screen, RED, ( int(self.x), int(self.y) ), PLAYER_SIZE)
         
-    def move_left(self):
-        self.x += -self.speed
+    def mover_left(self, speed):
+        self.x += -speed
     
-    def move_right(self):
-         self.x = SCREEN_WIDTH - ( SCREEN_WIDTH - (self.x + self.speed) )
+    def mover_right(self, speed):
+         self.x = SCREEN_WIDTH - ( SCREEN_WIDTH - (self.x + speed) )
+    
+    def Jump(self):
+        if self.isJump:
+            self.y -= self.speed * PLAYER_JUMP_SIZE
+            
+        self.speed = -self.speed
+        self.y += self.speed             
+        self.isJump = False
 
      
 #Clase banana 
-class Banana:
-    def __init__(self, screen, x, y, size, color):
+class Fruta:
+    def __init__(self, screen, x, y, vel, size, color):
         self.screen = screen
         self.x = x;
         self.y = y
+        self.vel = vel
         self.size = size
         self.color = color
-        self.speed = 0.5
-        
+
+#Clase para banana
+class Banana(Fruta):        
     def dibuja(self):
-        pygame.draw.circle(self.screen, BLUE, ( int(self.x), int(self.y) ), self.size)
+        pygame.draw.circle(self.screen, self.color, ( int(self.x), int(self.y) ), self.size)
     
-    def move(self):
-        self.y += self.speed
+    def mover(self):
+        self.y += self.vel
+ 
+#Clase para cocos       
+class Coco(Fruta):
+    def dibuja(self):
+        pygame.draw.circle(self.screen, self.color, ( int(self.x), int(self.y) ), self.size)
+    
+    def mover(self):
+        self.y += self.vel
+    
+    
+#Crea una fruta        
+def crearFruta(screen, color):
+    if color == BLUE:
+        x = random.randint(BANANA_SIZE, SCREEN_WIDTH - BANANA_SIZE )
+        y = BANANA_SIZE
+        fruta = Banana(screen, x, y, BANANA_SPEED, BANANA_SIZE, BLUE)
+    
+    elif color == WHITE:
+        x = random.randint(COCO_SIZE, SCREEN_WIDTH - COCO_SIZE )
+        y = COCO_SIZE    
+        fruta = Coco(screen, x, y, COCO_SPEED, COCO_SIZE, WHITE)
+    
+    return fruta
         
-        
+       
 #Crea una lista de bananas        
-def createBananas(screen, num_bananas):
+def createListaBananas(screen, num_bananas):
     my_bananas = []
 
     for i in range(num_bananas):
         x = random.randint(BANANA_SIZE, SCREEN_WIDTH - BANANA_SIZE )
         y = BANANA_SIZE
     
-        banana = Banana(screen, x, y, BANANA_SIZE, BLUE)
-        my_bananas.append(banana)
+#        banana = Banana(screen, x, y, BANANA_SIZE, BLUE, speed)
+#        my_bananas.append(banana)
         
     return my_bananas
 
 
-#Verifica la collision
-def collision(banana, player):
-     if (banana.x + BANANA_SIZE > player.x - PLAYER_SIZE) and (banana.x - BANANA_SIZE < player.x + PLAYER_SIZE):
-        if (banana.y + banana.size) > player.y - PLAYER_SIZE:
-            return True
-        else:
-            return False
+#Verifica la collision de una fruta con el monkey
+def colision(fruta, player):
+    colision = False
+    record = 0
+    
+    if fruta.color == WHITE:
+         if (fruta.x + COCO_SIZE > player.x - PLAYER_SIZE) and (fruta.x - COCO_SIZE < player.x + PLAYER_SIZE):
+            if (fruta.y + fruta.size) > player.y - PLAYER_SIZE:
+                colision = True
+                record = -1
+    
+    elif fruta.color == BLUE:
+        if (fruta.x + BANANA_SIZE > player.x - PLAYER_SIZE) and (fruta.x - BANANA_SIZE < player.x + PLAYER_SIZE):
+            if (fruta.y + fruta.size) > player.y - PLAYER_SIZE:
+                colision = True
+                record = 1
+              
+    return (colision, record)
+
 
 #Verifica si Player esta dentro de la pantalla
 def checkLimitX(player, direccion):
-    if direccion == 'LEFT' and (player.x - PLAYER_SIZE) >= 0:
+    if (direccion == 'LEFT') and (player.x - PLAYER_SIZE) >= 0:
         return True
-    elif direccion == 'RIGHT' and (player.x + PLAYER_SIZE) <= SCREEN_WIDTH:
+    elif (direccion == 'RIGHT') and (player.x + PLAYER_SIZE) <= SCREEN_WIDTH:
         return True
     else:
         return False
-
+  
+    
+#Verifica si la banana toco el piso
 def checkLimitY(banana):
     if banana.y > 0:
         if (banana.y + BANANA_SIZE) > SCREEN_HEIGHT:
-            banana.y = SCREEN_HEIGHT - BANANA_SIZE
+            #banana.y = SCREEN_HEIGHT - BANANA_SIZE
+            return True
--- Monkey_Game.py
+++ Monkey_Game.py
@@ -16,43 +16,63 @@ import math
 
 #Iniciamos Pygame
 pygame.init()
-screen = pygame.display.set_mode(playtools.SCREEN_SIZE)
+screen = pygame.display.set_mode(GameTools.SCREEN_SIZE)
 pygame.display.set_caption("THE MONKEY GAME")
 
 #Crea el monkey y las bananas
 monkey = Player(screen, SCREEN_WIDTH/2, SCREEN_HEIGHT - 20) #Creamos el Monkey al centro de la pantalla
-num_bananas = 20
-lista_bananas = [] 
-lista_bananas = createBananas(screen, num_bananas)   
+#num_bananas = 20
+#lista_bananas = [] 
+#lista_bananas = createListaBananas(screen, num_bananas)   
+
+lista_frutas = [] 
+
+#Objeto para registro del tiempo
+clock = pygame.time.Clock()
 
 #Ciclo infinito para dibujar
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit() #necesario para cerrar la ventana al presionar X sino se usa se traba 
+            pygame.quit() #necesario para cerrar la ventana al presionar X sino se usa se traba el juego
             running = False
             exit()
     
-    #Revisa que tecla due presionada
+    #Revisa que tecla fue presionada
     keys = pygame.key.get_pressed()
     if keys[K_LEFT]:
         if checkLimitX(monkey, 'LEFT'):
-            monkey.move_left()
+            monkey.mover_left(PLAYER_SPEED)
     
     elif keys[K_RIGHT]:
         if checkLimitX(monkey, 'RIGHT'):
-            monkey.move_right()
+            monkey.mover_right(PLAYER_SPEED)
     
     screen.fill((0, 0, 0))  
     
     monkey.dibuja() #Dibuja el monkey
-    for b in lista_bananas: #Dibuja y mueve las bananas
-        b.dibuja()
-        b.move()
-        checkLimitY(b)            
-        if collision(b, monkey):
-            lista_bananas.remove(b)
+    
+    un_coco = crearFruta(screen, WHITE) #Crea un coco
+    una_banana = crearFruta(screen, BLUE) #Crea una banana
+    lista_frutas.append(un_coco)
+    lista_frutas.append(una_banana)
+        
+    for f in lista_frutas:     #Dibuja y mueve las bananas
+        if checkLimitY(f):     #Revisa si la fruta ya alcanzo el piso de la pantalla       
+            lista_frutas.remove(f)    
+        else:
+            #revisa si hubo colision, actualiza el contador y la bandera de paro
+            (col, record) = colision(f, monkey) 
+            if col:
+                lista_frutas.remove(f)
+                
+        record += record   
+        f.dibuja()
+        f.mover()
+            
+    if monkey.isJump:
+        monkey.Jump()
       
     #Refresca la pantalla
     pygame.display.update()
